452b3e74c6105d3ecc2dc3827db9769469dc31b5
Jason Ingram <jasoningram952@gmail.com>
2021-06-19 15:39:39 -0700

    added main game loop

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+pygame.display.set_mode((900, 500 ) )
+# Variable to keep the main loop running
+running = True
+
+# Main loop
+while running:
+    # Look at every event in the queue
+    for event in pygame.event.get():
+        # Did the user hit a key?
+        if event.type == KEYDOWN:
+            print(key)
+            # Was it the Escape key? If so, stop the loop.
+            if event.key == K_ESCAPE:
+                running = False
+
+        # Did the user click the window close button? If so, stop the loop.
+        elif event.type == QUIT:
+            running = False
